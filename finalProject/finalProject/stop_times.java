185ff7c0ceff6a95d235bc3c7b8610cf969cbbeb
aines <aines@LAPTOP-60I1RSB6.fritz.box>
2022-04-10 19:46:02 +0100

    addition of insertion sort

--- finalProject/finalProject/stop_times.java
+++ finalProject/finalProject/stop_times.java
@@ -25,24 +25,25 @@ public class stop_times {
 			int j = 0;
 			while(scanner.hasNextLine()&&j<i+1)
 			{
-				
-					String line = scanner.nextLine().trim();
-					String[]key = line.split(",");
-					this.trip_id = key[0];
-					this.arrival_time = key[1];
-				
-					this.departure_time = key[2];
-					this.stop_id = key[3];
-					this.stop_sequence = key[4];
-					this.stop_headsign = key[5];
-					this.pickup_type = key[6];
-					this.dropoff_type = key[7];
-					if(key.length ==9)
-					{
+
+				String line = scanner.nextLine().trim();
+				String[]key = line.split(",");
+				this.trip_id = key[0];
+
+				this.arrival_time = key[1];
+
+				this.departure_time = key[2];
+				this.stop_id = key[3];
+				this.stop_sequence = key[4];
+				this.stop_headsign = key[5];
+				this.pickup_type = key[6];
+				this.dropoff_type = key[7];
+				if(key.length ==9)
+				{
 					this.shape_dist_traveled = key[8];
-					}
-					j++;
-				
+				}
+				j++;
+
 			}
 
 		}catch(Exception x)
@@ -53,50 +54,75 @@ public class stop_times {
 		}
 
 	}
-	//trip_id,arrival_time,departure_time,stop_id,stop_sequence,stop_headsign,pickup_type,drop_off_type,shape_dist_traveled
-
-	public static ArrayList<String> findArrivalTime(ArrayList<stop_times> stops,String arrivalTime)
+	static ArrayList<stop_times> insertionSort (ArrayList<stop_times>stops){
+			stop_times temporary;
+			for(int i = 0; i<stops.size(); i++)
+			{
+				for (int j = i; j > 0 ; j--)
+				{
+					int number1 =Integer.parseInt(stops.get(j).stop_id);
+					int number2 = Integer.parseInt(stops.get(j-1).stop_id);
+					if(number1>number2)
+					{
+				
+						temporary = stops.get(j);
+						stops.set(j, stops.get(j-1));
+						stops.set(j-1, temporary);
+					}
+				}
+			}
+			return(stops);	
+		}
+	
+	public static void findArrivalTime(ArrayList<stop_times> stops,String arrivalTime)
 	{
 		String str="";
 
-		ArrayList<String> details = new ArrayList<String>();
+		ArrayList<Integer> values = new ArrayList<Integer>();
+		
 		for(int i = 0; i<stops.size();i++)
 		{
+
 			if(stops.get(i).arrival_time.equals(" "+arrivalTime)||stops.get(i).arrival_time.equals(arrivalTime))
 			{
-				str=("trip id: "+stops.get(i).trip_id+", arrival time: "+stops.get(i).arrival_time+
-						", departure time: "+stops.get(i).departure_time +" stop_id: "+stops.get(i).stop_id+
-						", stop_sequence: " +stops.get(i).stop_sequence + ", stop headsign: "+ stops.get(i).stop_headsign+
-						", pickup type: "+stops.get(i).pickup_type+", dropoff type: "+stops.get(i).dropoff_type+
-						", shape distance travelled: "+stops.get(i).shape_dist_traveled);
-
-				details.add(str);
+				values.add(i);
 			}
 		}
-		return details;
+		ArrayList<stop_times> valuesStops = new ArrayList<stop_times>();
+		for(int i = 0;i<values.size();i++)
+		{
+			valuesStops.add(stops.get(values.get(i)));
+			insertionSort(valuesStops);
+		}
+		
+		for(int i = 0;i<values.size();i++)
+		{
+
+			System.out.println("trip id: "+valuesStops.get(i).trip_id+", arrival time: "+valuesStops.get(i).arrival_time+
+					", departure time: "+valuesStops.get(i).departure_time +" stop_id: "+valuesStops.get(i).stop_id+
+					", stop_sequence: " +valuesStops.get(i).stop_sequence + ", stop headsign: "+ valuesStops.get(i).stop_headsign+
+					", pickup type: "+valuesStops.get(i).pickup_type+", dropoff type: "+valuesStops.get(i).dropoff_type+
+					", shape distance travelled: "+valuesStops.get(i).shape_dist_traveled);
+
+		}
 
 	}
 
 	public static void main (String[]Args)
 	{
-	
+
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter arrival time (hh:mm:ss): ");
 		String arrivalTime = input.next();
 		ArrayList<stop_times> stops= new ArrayList<stop_times>();
-		for(int i = 1;i<=20000;i++)
+		for(int i = 1;i<=10000;i++)
 		{
 			stop_times newStop = new stop_times("stop_times.txt",i);
 			stops.add(newStop);
 		}
 
-		ArrayList<String>details = findArrivalTime(stops,arrivalTime);
-		for(int i = 0;i<details.size();i++)
-		{
-			System.out.println(details.get(i));
-		}
+		findArrivalTime(stops,arrivalTime);
 
-		
 	}
 }
 
